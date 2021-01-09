ba9ddc44b0f5fdb77c2a3f44d3cd4d8f17b4bda4
Chad Miller <chad.miller.3333@gmail.com>
2021-01-08 23:14:04 -0500

    #15: removed unused imports and variables

--- src/components/admin/Router.jsx
+++ src/components/admin/Router.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { Redirect, Route, Switch, useRouteMatch } from 'react-router-dom';
+import { Route, Switch, useRouteMatch } from 'react-router-dom';
 
 import CreateAestheticPage from './pages/CreateAestheticPage';
 import EditAestheticPage from './pages/EditAestheticPage';
 
 const Router = props => {
   const match = useRouteMatch();
-  const session = props.session;
 
   return (
     <Switch>
